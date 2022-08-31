8008809ce8cb15335f5a96096a350cf5e167c9e3
katerin <ckaterinarellano@gmail.com>
2022-08-30 23:35:53 -0500

    fix enviroment variable

--- src/services/users.js
+++ src/services/users.js
@@ -3,5 +3,6 @@ import apiFetch from "./api-fetch";
 export async function getUser() {
   const { ...user } = await apiFetch("/users");
   let users = Object.values(user);
+  console.log(user)
   return users;
 }
